40c15c3adbee5912bd0b25691aaaa3754b2e6c06
Eric B <eric.bezzam@epfl.ch>
2018-11-03 21:23:43 +0100

    Parse matrix dimensions.

--- action-writeEquationIntentParser.py
+++ action-writeEquationIntentParser.py
@@ -15,8 +15,13 @@ def user_give_integral(hermes, intent_message):
 
 
 def user_create_matrix(hermes, intent_message):
-    sentence = "Hello, you asked to create a matrix."
-    hermes.publish_end_session(intent_message.session_id, sentence)
+
+    first_dim = intent_message.slots.first_dim.first().value.value
+    second_dim = intent_message.slots.second_dim.first().value.value
+
+    tts = "Hello you asked to create a {} by {} matrix.".format(first_dim, second_dim)
+
+    hermes.publish_end_session(intent_message.session_id, tts)
 
 
 def intent_received(hermes, intent_message):
